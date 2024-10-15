b56b43152fe5041eccf50a20a85d2e01fd1c1920
MartinezKevin <kmartinez@liveventures.com>
2024-10-15 02:39:55 -0500

    Feat: Lastest changes

--- src/components/fields/HabeasDataField/index.tsx
+++ src/components/fields/HabeasDataField/index.tsx
@@ -12,7 +12,7 @@ export default function HabeasDataField() {
     <div className='flex flex-col gap-2'>
       <label htmlFor="habeas_data">
         <input type='checkbox' id='habeas_data' name='habeas_data' className='mr-2'/>
-        <span>Autorizo el tratamiento de mis datos de acuerdo con la finalidad establecida en la <Link href={"https://www.minjusticia.gov.co/ministerio/Documents/SIG/_pol%C3%ADtica%20de%20tratamiento%20y%20protecci%C3%B3n%20datos%20personales.pdf"} className='text-blue-600 underline'>política de protección de datos personales.</Link></span>
+        <span>Autorizo el tratamiento de mis datos de acuerdo con la finalidad establecida en la <Link target='_blank' href={"https://www.minjusticia.gov.co/ministerio/Documents/SIG/_pol%C3%ADtica%20de%20tratamiento%20y%20protecci%C3%B3n%20datos%20personales.pdf"} className='text-blue-600 underline'>política de protección de datos personales.</Link></span>
       </label>
       {meta.touched && meta.error ? <FormError name={field.name} /> : null}
     </div>
