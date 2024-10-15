bb1c7d5c72680d100e2f3ba820f9bc32dee5ff33
MartinezKevin <kmartinez@liveventures.com>
2024-10-15 02:39:26 -0500

    Feat: The habeas data field was added

--- src/components/fields/GeoField/index.tsx
+++ src/components/fields/GeoField/index.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useField } from 'formik'
 import React from 'react'
+import FormError from '../FormError'
 
 interface FormFieldProps{
   name: string,
@@ -13,7 +14,7 @@ interface FormFieldProps{
 
 export default function FormField({name, placeholder, options}: FormFieldProps) {
 
-  const [field] = useField(name)
+  const [field, meta] = useField(name)
 
   if(!options){
     return(
@@ -31,18 +32,21 @@ export default function FormField({name, placeholder, options}: FormFieldProps) 
   }
 
   return (
-    <label htmlFor={name} className='relative group w-full'>
-      <span className={"absolute -top-3 left-2 px-2 text-sm bg-black text-gray"}>
-        {placeholder}
-      </span>
-      <select 
-        {...field}
-        id={name}
-        name={name}
-        className='bg-transparent bg-red text-white border-[1px] border-gray-400 w-full outline-0 ring-0 px-4 h-14 rounded-lg'>
-        <option value={""} className='text-black'>-- Selecciona un Departamento --</option>
-        {options?.map((option, i)=>(<option key={i} value={option?.id} className='text-black'>{option?.name}</option>))}
-      </select>
-    </label>
+    <div className='flex flex-col gap-2'>
+      <label htmlFor={name} className='relative group w-full'>
+        <span className={"absolute -top-3 left-2 px-2 text-sm bg-black text-gray"}>
+          {placeholder}
+        </span>
+        <select 
+          {...field}
+          id={name}
+          name={name}
+          className='bg-transparent bg-red text-white border-[1px] border-gray-400 w-full outline-0 ring-0 px-4 h-14 rounded-lg'>
+          <option value={""} className='text-black'>-- Selecciona un Departamento --</option>
+          {options?.map((option, i)=>(<option key={i} value={option?.id} className='text-black'>{option?.name}</option>))}
+        </select>
+      </label>
+      {meta.touched && meta.error ? <FormError name={field.name} /> : null}
+    </div>
   )
 }
